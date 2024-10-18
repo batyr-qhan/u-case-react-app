8c71828b61e8671238501538dc5533bb8d7ceec6
nthroneo <batyrqh@protonmail.com>
2024-10-18 18:21:27 +0600

    Add initial project files and structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+// import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="../assets/logo.svg" alt="logo" />
+      <img src="/react.svg" alt="react logo" />
+    </div>
+  );
+};
+
+export default Header;
